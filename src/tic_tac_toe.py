3dd88922c806d0de3459983818fe95e20d007d61
Jesus Magana <maganazero5@gmail.com>
2018-01-04 19:10:29 -0800

    feat: Added end condition
    Game now detects win and draw conditions. Unit tests also added for end function.

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -6,6 +6,7 @@ PLAYER_TWO = 1
 EDGE = "-------"
 SPACE = " "
 INPUT_STRING = "|"
+VICTORY = [[1,4,7],[2,5,8],[3,6,9],[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7]]
 
 #Return Board State as a string
 def get_board_string(board_state):
@@ -40,32 +41,76 @@ def get_next_value(player, board_state):
         invalid_value = value < 1 or value > 9
     return value
 
+# Update the board state with the users input
 def update_board_state(board_state, next_play, player):
     if player == PLAYER_ONE:
         player_char = "X"
     else:
         player_char = "O"
     next_play_str = str(next_play)
+    # We assume the index is valid since we filtered during input
     loc = board_state.index(next_play_str)
     board_state[loc] = player_char
     return board_state
 
+# Check if out of turns
+def check_end_game(next_play, num_turns, player_moves):
+    draw = num_turns >= 9
+    cont = True
+    # Only check for victory if we didn't draw
+    if not draw:
+        # Get possible victory conditions
+        new_vic_cond = [state for state in VICTORY if next_play in state]
+
+        # For each no possible victory state
+        for state in new_vic_cond:
+            count = 0
+            # Check if win condition is present
+            for val in state:
+                if val in player_moves:
+                    count += 1
+            # Player wins
+            if count == 3:
+                cont = False
+                break
+    else:
+        cont = False
+    return cont
+
 #Main loop
 def main():
     not_finished = True
+    moves = [[],[]]
     player = PLAYER_ONE
+    p_move = moves[player]
     board_state = range(1,10)
     board_state = [str(val) for val in board_state]
+    count = 1
+
     # Infinite loop
     while not_finished:
         #1. Print board state
         print_board_state(board_state)
         #2. Get player input
         next_play = get_next_value(player, board_state)
+
         #3. Replace value with player movement
         board_state = update_board_state(board_state, next_play, player)
-        #4. Move to next player
-        player = (player + 1) % 2
+        moves[player].append(next_play)
+
+        #4. Check victory condition
+        not_finished = check_end_game(next_play, count, p_move)
+        count += 1
+
+        if not_finished:
+            #5. Move to next player
+            player = (player + 1) % 2
+            p_move = moves[player]
+    if count == 9:
+        end_str = "Draw!"
+    else:
+        end_str = "Player %d: won!" %(player + 1)
+    print end_str
 
 #Call main if not imported
 if __name__ == "__main__":
--- tests/tic_tac_toe_unit_test.py
+++ tests/tic_tac_toe_unit_test.py
@@ -48,5 +48,89 @@ class Test_Get_Board_String(unittest.TestCase):
         return_board = tic_tac_toe.get_board_string(test_board)
         self.assertEqual(return_board, target_board)
 
+class Test_Check_End_Game(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    # Test no values
+    def test_empty_set(self):
+        test_moves = []
+        num_turns = 0
+        next_play = 1
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(True, cont)
+
+    # Test no values
+    def test_full_set(self):
+        test_moves = [1,2,3,4,5,6,7,8,9]
+        num_turns = 0
+        next_play = 9
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_1(self):
+        test_moves = [1,2,3]
+        num_turns = 0
+        next_play = 3
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_2(self):
+        test_moves = [4,5,6]
+        num_turns = 0
+        next_play = 6
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_3(self):
+        test_moves = [7,8,9]
+        num_turns = 0
+        next_play = 9
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_4(self):
+        test_moves = [1,4,7]
+        num_turns = 0
+        next_play = 7
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_5(self):
+        test_moves = [2,5,8]
+        num_turns = 0
+        next_play = 8
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_6(self):
+        test_moves = [3,6,9]
+        num_turns = 0
+        next_play = 9
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_7(self):
+        test_moves = [1,5,9]
+        num_turns = 0
+        next_play = 9
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
+
+    # Test no values
+    def test_set_8(self):
+        test_moves = [3,5,7]
+        num_turns = 0
+        next_play = 7
+        cont = tic_tac_toe.check_end_game(next_play, num_turns, test_moves)
+        self.assertEqual(False, cont)
 if __name__ == '__main__':
     unittest.main()
